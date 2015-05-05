989fc65c72931855e264abe8c36f158c1ce8a7a7
SergioRodriguezRuiz <serrodrui@gmail.com>
2015-05-05 17:45:47 +0200

    Add libs and solve dependencies require

--- js/config.js
+++ js/config.js
@@ -4,10 +4,20 @@
 require.config({
     deps: ['main'],
     paths: {
+        jquery: 'libs/jquery',
+        underscore: 'libs/underscore',
         backbone: 'libs/backbone',
+        text: 'libs/text',
         bootstrap: 'libs/bootstrap'
     },
     shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: ['underscore','jquery'],
+            exports: 'backbone'
+        },
         'bootstrap': ['jquery']
     }
 })
